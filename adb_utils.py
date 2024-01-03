4592a4276e985808623f6fc6366f50e6d77fe9a3
Vyacheslav Shumkov <vyacheslavshumkov@gmail.com>
2024-01-04 04:06:42 +0500

    feat: change ppadb to direct adb

--- adb_utils.py
+++ adb_utils.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from ppadb.client import Client as AdbClient
+import subprocess
 
 
 class AdbAutomation:
@@ -8,37 +8,41 @@ class AdbAutomation:
     A class to abstract the interactions with an Android device using ADB.
     """
 
-    def __init__(self, host="127.0.0.1", port=5038):
-        # Load the .env file and get the DEVICE_ID
+    def __init__(self):
+        """
+        Load the .env file and get the DEVICE_ID.
+        """
         load_dotenv()
-        device_id = os.getenv("DEVICE_ID")
+        self.device_id = os.getenv("DEVICE_ID")
 
-        if device_id is None:
+        if self.device_id is None:
             raise ValueError("DEVICE_ID not found in .env file")
 
-        client = AdbClient(host=host, port=port)
-        device = client.device(device_id)
-
-        if device is None:
-            raise Exception(f"No device found with ID {device_id}")
-
-        self.device = device
+    def run(self, command):
+        """
+        Executes an ADB command on the specified device.
+        """
+        try:
+            subprocess.run(f"adb -s {self.device_id} shell {command}",
+                           capture_output=True, text=True, shell=True)
+        except Exception as e:
+            print(f"Error running command: {command}, error message: {e}")
 
     def tap(self, x, y):
         """
         Simulates a tap on the device screen at coordinates (x, y).
         """
         try:
-            self.device.shell(f"input tap {x} {y}")
+            self.run(f"input tap {x} {y}")
         except Exception as e:
-            print(f"Error taping {x} {y}, error was: {e}")
+            print(f"Error taping {x} {y}, error message: {e}")
 
     def input_text(self, text):
         """
         Inputs the specified text into the active field on the device.
         """
         try:
-            self.device.shell(f"input text {text}")
+            self.run(f"input text {text}")
         except Exception as e:
             print(f"Error while inputting text: {e}")
 
@@ -47,9 +51,9 @@ class AdbAutomation:
         Simulates the pressing of a key event.
         """
         try:
-            self.device.shell(f"input keyevent {keyevent_code}")
+            self.run(f"input keyevent {keyevent_code}")
         except Exception as e:
-            print(f"Error while pressing keyevent {keyevent_code}, error was: {e}")
+            print(f"Error while pressing keyevent {keyevent_code}, error message: {e}")
 
     def start_app(self, package_name, activity_name=None):
         """
@@ -57,8 +61,8 @@ class AdbAutomation:
         """
         try:
             if activity_name:
-                self.device.shell(f"am start -n {package_name}/{activity_name}")
+                self.run(f"am start -n {package_name}/{activity_name}")
             else:
-                self.device.shell(f"am start -a android.intent.action.MAIN -n {package_name}")
+                self.run(f"am start -a android.intent.action.MAIN -n {package_name}")
         except Exception as e:
-            print(f"Error while starting an app, error was: {e}")
+            print(f"Error while starting an app, error message: {e}")
